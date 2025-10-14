30642ceca1b4d54b15574e04d7e3c1c8788291a6
Robson Aguiar <robson.aguiar@dedalusprime.com.br>
2025-10-14 08:38:32 -0300

    Move Instagram to contact section below WhatsApp with gradient style

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,8 @@ output "resource_group_name" {
   description = "Nome do Resource Group"
   value       = azurerm_resource_group.rg.name
 }
+
+output "cdn_endpoint_url" {
+  description = "URL do CDN Endpoint"
+  value       = "https://${azurerm_cdn_endpoint.cdn_endpoint.host_name}"
+}
